5ad72bc32c0a4f5cae73e7965490b9877078b4f5
Harit Kapadia <haritkapadia@rocketmail.com>
2020-08-22 14:41:02 -0400

    Footer text scales

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import {
 	Button,
-	Text,
 	Footer,
 	FooterTab,
 	Content,
@@ -15,8 +14,10 @@ import {
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
 const styles = StyleSheet.create({
+	footer: {
+		height: 70
+	},
 	icon: {
-		backgroundColor: "#5e97da",
 		padding: 5,
 		color: "#ffffff",
 	},
@@ -24,6 +25,9 @@ const styles = StyleSheet.create({
 		display: "flex",
 		flexDirection: "row",
 		justifyContent: "space-between",
+	},
+	footerText: {
+		color: "#ffffff",
 	},
 });
 
@@ -44,19 +48,19 @@ const Page = ({ navigation, children }) => (
 			<FooterTab style={{ backgroundColor: "#5e97da" }}>
 				<Button vertical onPress={() => navigation.navigate("Share")}>
 					<FontAwesome5 name="share" size={20} style={styles.icon} />
-					<Text>Share</Text>
+					<Text adjustsFontSizeToFit style={styles.footerText}>Share</Text>
 				</Button>
 				<Button vertical onPress={() => navigation.navigate("Home")}>
 					<FontAwesome5 name="home" size={20} style={styles.icon} />
-					<Text>Home</Text>
+					<Text adjustsFontSizeToFit style={styles.footerText}>Home</Text>
 				</Button>
 				<Button vertical onPress={() => navigation.navigate("Friends")}>
 					<FontAwesome5 name="users" size={20} style={styles.icon} />
-					<Text>Friends</Text>
+					<Text adjustsFontSizeToFit style={styles.footerText}>Friends</Text>
 				</Button>
 				<Button vertical onPress={() => navigation.navigate("Map")}>
-					<FontAwesome5 name="map" style={styles.icon} />
-					<Text>Map</Text>
+					<FontAwesome5 name="map" size={20} style={styles.icon} />
+					<Text adjustsFontSizeToFit style={styles.footerText}>Map</Text>
 				</Button>
 			</FooterTab>
 		</Footer>
