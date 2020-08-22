2c9a9d685559ff3a08f5d34a3fc7ad411ca217fa
Harit Kapadia <haritkapadia@rocketmail.com>
2020-08-21 23:45:17 -0400

    Added navigation

--- App.tsx
+++ App.tsx
@@ -1,5 +1,10 @@
-import React,{useState} from "react";
-import { StyleSheet,View,TouchableOpacity } from 'react-native';
+// "...add the following at the *top* of your entry file..."
+import "react-native-gesture-handler";
+
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
 import ToastExample from "./ToastExample";
 // test
 import {
@@ -16,8 +21,25 @@ import {
 	Input,
 } from "native-base";
 
+const Stack = createStackNavigator();
+
+const A = () => (
+	<Content>
+		<Item rounded>
+			<Input keyboardType="numeric" placeholder="Running distance" />
+		</Item>
+		<View style={styles.container}>
+			<TouchableOpacity
+				style={styles.button}
+				onPress={() => ToastExample.show("Button clicked!", ToastExample.SHORT)}
+			>
+				<Text>click me!</Text>
+			</TouchableOpacity>
+		</View>
+	</Content>
+);
+
 const App = () => {
-	const[count,setCount]= useState(0);
 	return (
 		<Container>
 			<Header>
@@ -25,40 +47,39 @@ const App = () => {
 					<Title>Running App</Title>
 				</Body>
 			</Header>
-			<Content>
-				<Item rounded>
-					<Input keyboardType="numeric" placeholder="Running distance" />
-				</Item>
-				<View style={styles.container}>
-					<TouchableOpacity
-						style={styles.button}
-						onPress={()=>{
-							setCount(count+1);
-							ToastExample.show('button clicked!',ToastExample.SHORT)
-						}}
-					>
-						<Text>click me!</Text>
-					</TouchableOpacity>
-					<Text>clicked {count} times</Text>
-				</View>
-			</Content>
+			<NavigationContainer>
+				<Stack.Navigator>
+					<Stack.Screen
+					name="Home"
+					component={A}
+					options={{title: "Home"}}
+					/>
+					<Stack.Screen
+					name="Friends"
+					component={A}
+					options={{title: "Friends"}}
+					/>
+				</Stack.Navigator>
+			</NavigationContainer>
 		</Container>
 	);
 };
+
 const styles = StyleSheet.create({
 	content: {
 		display: "flex",
 	},
 	button: {
-		alignItems:'center',
-		backgroundColor:'#dddddd',
-		padding:10,
+		alignItems: "center",
+		backgroundColor: "#dddddd",
+		padding: 10,
 		marginBottom:10
 	},
-		container:{
-			flex:1,
-			justifyContent:'center',
-			alignItems:'center'
+		container: {
+			flex: 1,
+			justifyContent: "center",
+			alignItems: "center"
 		},
 });
+
 export default App;
