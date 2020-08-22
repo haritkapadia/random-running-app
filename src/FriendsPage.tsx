f9d7e53e60151163b81ec3e45ee467d1a95b9a3b
Julia Chen <juliazychen@gmail.com>
2020-08-22 14:28:44 -0400

    Added friends page

--- src/FriendsPage.tsx
+++ src/FriendsPage.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import { Text } from "native-base";
+import { Text, Container, Header, Tab, Tabs } from "native-base";
 import Page from "./Page";
+import FriendsList from './FriendsList';
+import Leaderboard from "./Leaderboard";
 
-const FriendsPage = ({navigation}) => (
+const FriendsPage = ({ navigation }) => (
 	<Page navigation={navigation}>
-		<Text>Friends OwO</Text>
+		<Container>
+			<Tabs>
+				<Tab heading="Friends">
+					<FriendsList />
+				</Tab>
+				<Tab heading="Leaderboard">
+					<Leaderboard />
+				</Tab>
+			</Tabs>
+		</Container>
 	</Page>
 );
 
---
+++ src/Leaderboard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, Container, Tab, Tabs } from "native-base";
+import Page from "./Page";
+
+const Leaderboard = () => (
+    <Container>
+        <Text>Owo I'm first</Text>
+    </Container>
+);
+
+export default Leaderboard;
