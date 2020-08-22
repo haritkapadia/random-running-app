32a2c020d3d4c8de989d2f3de8a789e9a12b395f
Harit Kapadia <haritkapadia@rocketmail.com>
2020-08-21 21:52:19 -0400

    Fixed indentation settings

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((js-basic-offset . 4)
+         (sgml-basic-offset . 4)
+         (indent-tabs-mode . t))))
--- App.tsx
+++ App.tsx
@@ -4,11 +4,11 @@ import { View } from 'react-native';
 import Greeting from "./src/Greeting";
 
 const App = () => {
-  return (
-      <View>
-          <Greeting />
-      </View>
-  );
+	return (
+		<View>
+			<Greeting />
+		</View>
+	);
 };
 
 export default App;
