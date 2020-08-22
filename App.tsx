91b3a8b646447b568e87a361f8731e4d4f7e1c13
Harit Kapadia <haritkapadia@rocketmail.com>
2020-08-21 22:31:46 -0400

    Used native-base things

--- App.tsx
+++ App.tsx
@@ -1,10 +1,36 @@
 import React from "react";
-import { Container, Text } from "native-base";
+import {
+	Container,
+	Text,
+	Icon,
+	Header,
+	Footer,
+	Content,
+	Left,
+	Right,
+	Body,
+	Button,
+	Title,
+} from "native-base";
 
 const App = () => {
     return (
 		<Container>
-			<Text>New hi UwU</Text>
+			<Header>
+				<Left>
+					<Text>Menu</Text>
+				</Left>
+				<Body>
+					<Title>Running App</Title>
+				</Body>
+				<Right />
+			</Header>
+			<Content>
+				<Text>UWU OwO UWU</Text>
+			</Content>
+			<Footer>
+				<Text>Foot</Text>
+			</Footer>
 		</Container>
     );
 };
