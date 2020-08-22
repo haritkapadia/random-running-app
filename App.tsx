dca507041e3321d920997e5373366b5f65373e7e
Julia Chen <juliazychen@gmail.com>
2020-08-22 12:54:06 -0400

    Updated transition

--- App.tsx
+++ App.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator, TransitionPresets, CardStyleInterpolators } from "@react-navigation/stack";
+import {
+	Container,
+	Body,
+	Title,
+	Header,
+} from "native-base";
 import RunningPage from "./src/RunningPage";
 import FriendsPage from "./src/FriendsPage";
 import SharePage from "./src/SharePage";
-
 const Stack = createStackNavigator();
 
-export default () => (
+
+
+export default () =>
 	<NavigationContainer>
 		<Stack.Navigator screenOptions={{ headerShown: false }}>
-			<Stack.Screen name="Home" component={RunningPage} />
-			<Stack.Screen name="Share" component={SharePage} />
-			<Stack.Screen name="Friends" component={FriendsPage} />
+			<Stack.Screen name="Home" component={RunningPage} options={{ cardStyleInterpolator: CardStyleInterpolators.forModalPresentationIOS }} />
+			<Stack.Screen name="Share" component={SharePage} options={{ cardStyleInterpolator: CardStyleInterpolators.forModalPresentationIOS }} />
+			<Stack.Screen name="Friends" component={FriendsPage} options={{ cardStyleInterpolator: CardStyleInterpolators.forModalPresentationIOS }} />
 		</Stack.Navigator>
 	</NavigationContainer>
-);
