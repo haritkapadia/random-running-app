9d5eb70fed8608de7aaae07b8245d26dbd6aace8
Julia Chen <juliazychen@gmail.com>
2020-08-22 22:15:11 -0400

    Updated profile page

--- src/ProfilePage.tsx
+++ src/ProfilePage.tsx
@@ -1,18 +1,37 @@
 import React from "react";
-import { Text, Container, Content, List, ListItem, Left, Right, Body, Thumbnail } from "native-base";
-import { StyleSheet, Image } from "react-native";
+import { Text, Container, Content, Button } from "native-base";
+import { StyleSheet, Image, View } from "react-native";
 import Page from "./Page";
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        alignItems: "center",
     },
+    image: {
+        width: 300,
+        height: 300,
+        marginTop: 20,
+        borderRadius: 300 / 2,
+        borderWidth: 5,
+        borderColor: "#5e97da"
+    },
+    button: {
+        padding: 5,
+        marginTop: 20
+    }
 });
 const ProfilePage = ({ navigation }) => (
     <Page navigation={navigation}>
-        <Content>
-            <Thumbnail circle large source={{ uri: 'https://cdn.discordapp.com/attachments/347043966476353538/746779456152272916/unknown.png' }} />
-        </Content>
+        <View style={styles.container}>
+            <Image
+                source={{ uri: 'https://cdn.discordapp.com/attachments/347043966476353538/746779456152272916/unknown.png' }}
+                style={styles.image}
+            />
+            <Button rounded style={styles.button}>
+                <Text>Upload Photo</Text>
+            </Button>
+        </View>
     </Page>
 );
 
