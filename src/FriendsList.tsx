f88a75165d8b897a4ad7dde940a75f007f236a2a
Julia Chen <juliazychen@gmail.com>
2020-08-23 00:12:37 -0400

    Edit friends and leaderboard

--- src/FriendsList.tsx
+++ src/FriendsList.tsx
@@ -14,34 +14,98 @@ const FriendsList = () => (
             <List>
                 <ListItem avatar>
                     <Left>
-                        <Thumbnail source={{ uri: "https://cdn.discordapp.com/attachments/347043966476353538/746779456152272916/unknown.png" }} />
+                        <Thumbnail source={{ uri: "https://www.mantruckandbus.com/fileadmin/_processed_/2/c/csm_kleiss-interview-header_f8b7588730.jpg" }} />
                     </Left>
                     <Body>
-                        <Text>Harit Kapadia</Text>
+                        <Text>Wyatt Smith</Text>
                     </Body>
                 </ListItem>
                 <ListItem avatar>
                     <Left>
-                        <Thumbnail source={{ uri: "https://cdn.discordapp.com/attachments/347043966476353538/746779456152272916/unknown.png" }} />
+                        <Thumbnail source={{ uri: "https://images.unsplash.com/photo-1534751516642-a1af1ef26a56?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" }} />
                     </Left>
                     <Body>
-                        <Text>David Jacewicz</Text>
+                        <Text>Natalie Wang</Text>
                     </Body>
                 </ListItem>
                 <ListItem avatar>
                     <Left>
-                        <Thumbnail source={{ uri: "https://cdn.discordapp.com/attachments/347043966476353538/746779456152272916/unknown.png" }} />
+                        <Thumbnail source={{ uri: "https://images.thestar.com/1L3hG8JS_mLmJsB1HAsC5nT1Ass=/1280x1024/smart/filters:cb(1593142732590)/https://www.thestar.com/content/dam/thestar/life/travel/2020/06/25/do-you-need-a-scenery-change/icefields_parkway_debbie_olsen.jpg" }} />
                     </Left>
                     <Body>
-                        <Text>Junyi Kapadia</Text>
+                        <Text>Gleb Abel</Text>
                     </Body>
                 </ListItem>
                 <ListItem avatar>
                     <Left>
-                        <Thumbnail source={{ uri: "https://cdn.discordapp.com/attachments/347043966476353538/746779456152272916/unknown.png" }} />
+                        <Thumbnail source={{ uri: "https://cdn.discordapp.com/attachments/347043966476353538/746848370605293599/Untitsdsled.png" }} />
                     </Left>
                     <Body>
-                        <Text>Wang Kapadia</Text>
+                        <Text>Helena Opeyemi</Text>
+                    </Body>
+                </ListItem>
+                <ListItem avatar>
+                    <Left>
+                        <Thumbnail source={{ uri: "https://sadanduseless.b-cdn.net/wp-content/uploads/2018/01/rabbit4.jpg" }} />
+                    </Left>
+                    <Body>
+                        <Text>Michelle Allison</Text>
+                    </Body>
+                </ListItem>
+                <ListItem avatar>
+                    <Left>
+                        <Thumbnail source={{ uri: "https://randomuser.me/api/portraits/men/56.jpg" }} />
+                    </Left>
+                    <Body>
+                        <Text>Edda Zambrano</Text>
+                    </Body>
+                </ListItem>
+                <ListItem avatar>
+                    <Left>
+                        <Thumbnail source={{ uri: "https://images.unsplash.com/photo-1489533119213-66a5cd877091?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80" }} />
+                    </Left>
+                    <Body>
+                        <Text>Gabriel Villanueva</Text>
+                    </Body>
+                </ListItem>
+                <ListItem avatar>
+                    <Left>
+                        <Thumbnail source={{ uri: "https://cdn.discordapp.com/attachments/347043966476353538/746848370605293599/Untitsdsled.png" }} />
+                    </Left>
+                    <Body>
+                        <Text>Tom Smirnov</Text>
+                    </Body>
+                </ListItem>
+                <ListItem avatar>
+                    <Left>
+                        <Thumbnail source={{ uri: "https://cdn.discordapp.com/attachments/347043966476353538/746848370605293599/Untitsdsled.png" }} />
+                    </Left>
+                    <Body>
+                        <Text>Miranda Curtis</Text>
+                    </Body>
+                </ListItem>
+                <ListItem avatar>
+                    <Left>
+                        <Thumbnail source={{ uri: "https://pbs.twimg.com/profile_images/1188507013233479681/WuNwaQ8R_400x400.jpg" }} />
+                    </Left>
+                    <Body>
+                        <Text>Samuel Evans</Text>
+                    </Body>
+                </ListItem>
+                <ListItem avatar>
+                    <Left>
+                        <Thumbnail source={{ uri: "https://specials-images.forbesimg.com/imageserve/1024724898/960x0.jpg?fit=scale" }} />
+                    </Left>
+                    <Body>
+                        <Text>Jacob Lombardi</Text>
+                    </Body>
+                </ListItem>
+                <ListItem avatar>
+                    <Left>
+                        <Thumbnail source={{ uri: "https://i2.wp.com/thepointsguy.com/wp-content/uploads/2020/04/GettyImages-1077097080.jpg?fit=2210%2C1357px&ssl=1" }} />
+                    </Left>
+                    <Body>
+                        <Text>Liam Singh</Text>
                     </Body>
                 </ListItem>
             </List>
--- src/LeaderboardPage.tsx
+++ src/LeaderboardPage.tsx
@@ -5,17 +5,23 @@ import Leaderboard from "react-native-leaderboard";
 import Page from "./Page";
 
 const data = [
-	{ userName: "Joe", highScore: 52, icon: "https://cdn.discordapp.com/attachments/347043966476353538/746779456152272916/unknown.png" },
-	{ userName: "Jenny", highScore: 120, icon: "https://cdn.discordapp.com/attachments/347043966476353538/746779456152272916/unknown.png" },
+    { userName: "Tom", highScore: 12462, icon: "https://cdn.discordapp.com/attachments/347043966476353538/746848370605293599/Untitsdsled.png" },
+    { userName: "Miranda", highScore: 1200, icon: "https://cdn.discordapp.com/attachments/347043966476353538/746848370605293599/Untitsdsled.png" },
+    { userName: "Jacob", highScore: 52, icon: "https://specials-images.forbesimg.com/imageserve/1024724898/960x0.jpg?fit=scale" },
+    { userName: "Liam", highScore: 5623, icon: "https://i2.wp.com/thepointsguy.com/wp-content/uploads/2020/04/GettyImages-1077097080.jpg?fit=2210%2C1357px&ssl=1" },
+    { userName: "Gleb", highScore: 2005, icon: "https://images.thestar.com/1L3hG8JS_mLmJsB1HAsC5nT1Ass=/1280x1024/smart/filters:cb(1593142732590)/https://www.thestar.com/content/dam/thestar/life/travel/2020/06/25/do-you-need-a-scenery-change/icefields_parkway_debbie_olsen.jpg" },
+    { userName: "Samuel", highScore: 3331, icon: "https://pbs.twimg.com/profile_images/1188507013233479681/WuNwaQ8R_400x400.jpg" },
+    { userName: "Natalie", highScore: 2564, icon: "https://images.unsplash.com/photo-1534751516642-a1af1ef26a56?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" },
+    { userName: "Wyatt", highScore: 8935, icon: "https://www.mantruckandbus.com/fileadmin/_processed_/2/c/csm_kleiss-interview-header_f8b7588730.jpg" },
 ];
 
 const LeaderboardPage = () => (
-	<Leaderboard
-	data={data}
-	sortBy="highScore"
-	labelBy="userName"
-	icon="icon"
-	/>
+    <Leaderboard
+        data={data}
+        sortBy="highScore"
+        labelBy="userName"
+        icon="icon"
+    />
 );
 
 export default LeaderboardPage;
